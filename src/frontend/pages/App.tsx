873f7612626a46bb1ceb502c9f30b0eee6e15ab2
Yasin Onur Yücel <yoy1992@gmail.com>
2025-08-05 21:43:51 +0200

    feat: add App component

---
+++ src/frontend/pages/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SwaggerComponent from '../components/SwaggerUI';
+
+function App() {
+  return (
+    <div className="App">
+      <SwaggerComponent />
+    </div>
+  );
+}
+
+export default App;
