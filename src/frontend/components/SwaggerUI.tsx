ffcb952eda752e641fa19a982747717c511d8944
Yasin Onur Yücel <yoy1992@gmail.com>
2025-08-05 22:05:35 +0200

    refactor: move frontend folder to nearacles-protocol/frontend

--- src/frontend/components/SwaggerUI.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
-import swaggerDocument from '../swagger.json';
-
-const SwaggerComponent = () => <SwaggerUI spec={swaggerDocument} />;
-
-export default SwaggerComponent;
